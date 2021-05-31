93f5088027d23e1aeac2a0e7452eabffafd8f600
Abe Hemati <abehemati@gmail.com>
2021-05-31 15:27:45 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your code here!
+
+document.querySelector('main').remove();
+
+let newHeader = document.createElement('h1');
+document.newHeader.appendChild('h1');
+
+newHeader.id = 'victory';
+
+newHeader.innerHTML = "Neo is the champion"
+
+
+
+
+
+
+
+
+
+
+
+
